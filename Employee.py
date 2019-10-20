8fdea2ed53c4078253c589c34b1558e1f313c1af
chaithanya kumar <chaithanyak192@gmail.com>
2019-10-20 14:39:45 +0530

    Employee data

---
+++ Employee.py
@@ -0,0 +1,16 @@
+class Employee:
+	def __init__(self,eno,ename,esal):
+		self.eno=eno
+		self.ename=ename
+		self.esal=esal
+	def display(self):
+		print("The employee name is:",self.ename)
+		print("The employee no	is:",self.eno)
+		print("The employee salary:" ,self.esal)
+class Increment():
+	def modify(self):
+		self.esal=self.esal+10000
+		self.display()
+		
+e=Employee(10,"durga",50000)
+Increment.modify(e)
